117aa4ee9f58f6357eb3d9531a625a62a6de14c8
Andres Castro <castroandres92@gmail.com>
2023-02-12 15:34:43 +0100

    Commit at:

--- python_scripts/data_process.py
+++ python_scripts/data_process.py
@@ -2,6 +2,7 @@
 # It will be called by the analysis jupyter notebook.
 
 import pandas as pd
+import geopandas as gpd
 
 def get_offices_location (data):
     """
@@ -21,4 +22,25 @@ def get_offices_location (data):
     df = pd.DataFrame(nested_data)
     df = df.dropna(subset=["office latitude", "office longitude"], how='all')
     print("Received data for ", len(nested_data), " companies. \n", df.shape[0], " companies with full location information left.")
+    return df
+
+def load_cities(city_names):
+    cities = {}
+    for city_name in city_names:
+        city_geo = gpd.read_file(f"data/geojsons/{city_name}.geojson")
+        cities[city_name.title()] = city_geo.geometry[0]
+    return cities
+
+def add_city_name(df, cities):
+    city_names = []
+    for _, row in df.iterrows():
+        lat, lon = row['office latitude'], row['office longitude']
+        point = gpd.points_from_xy([lon], [lat])
+        for city_name, city_geometry in cities.items():
+            if point.within(city_geometry)[0]:
+                city_names.append(city_name)
+                break
+        else:
+            city_names.append(None)
+    df['city'] = city_names
     return df
