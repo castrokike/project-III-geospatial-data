803c67b2276a560a9231adf06945de4f02f47654
Andres Castro <castroandres92@gmail.com>
2023-02-13 16:59:59 +0100

    Commit at:

--- python_scripts/foursquare.py
+++ python_scripts/foursquare.py
@@ -91,4 +91,48 @@ basket = process_4sq_data(bakset_raw)
 ## Query for vegan restaurants
 # Vegan and vegetarian restaurants within 5KM (our CEO can drive) of the office. Top 20 sorted by distance.
 
+url_vegan = "https://api.foursquare.com/v3/places/search?ll=37.7824%2C-122.4039&radius=5000&categories=13377&sort=DISTANCE&limit=20"
+
+headers = {
+    "accept": "application/json",
+    "Authorization": fsq_tok
+}
+
+response_vegan = requests.get(url_vegan, headers=headers)
+vegan_raw = response_vegan.json()
+vegan = process_4sq_data(vegan_raw)
+
+
+## Pet Grooming
+# Top 20 (by distance) pet grooming services on a 1.5Km radius from the office
+
+url_pet = "https://api.foursquare.com/v3/places/search?ll=37.7824%2C-122.4039&radius=1500&categories=11134&sort=DISTANCE&limit=20"
+
+headers = {
+    "accept": "application/json",
+    "Authorization": fsq_tok
+}
+
+response_pet = requests.get(url_pet, headers=headers)
+pet_raw = response_pet.json()
+pet = process_4sq_data(pet_raw)
+
+
+## Lets create a single data frame with all our nearby spots of interest:
+# First lets add a distinction to each set
+pet["group"] = "Pet Grooming"
+vegan["group"] = "Vegan Food"
+basket["group"] = "Basket court"
+starbucks["group"] = "Starbucks"
+schools["group"] = "Schools"
+airports["group"] = "Airports"
+
+# Concatenate all DataFrames to a single DataFrame:
+near_office = pd.concat([pet, vegan, basket, starbucks, schools, airports], axis=0)
+
+# Finally, exports the complete DataFrame
+near_office.to_csv('data/venues_near_office.csv')
+
+
+
 
